0a5208743fa9060920eeb117c216dad7814ec0fe
alken <5488673-alken@users.noreply.gitlab.com>
2026-02-25 05:44:51 +0000

    Complete assignment 5 part 2 with full credit to another student

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -1,3 +1,5 @@
+# The following lines relating to the server directory are attributed to Piyush Nagpal
+# (https://github.com/cu-ecen-aeld/assignment-5-its-me-piyush/blob/main/base_external/package/aesd-assignments/aesd-assignments.mk)
 
 ##############################################################
 #
@@ -6,7 +8,7 @@
 ##############################################################
 
 #DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = ecb824dea41f2aba4456113adbb4a561e317efde
+AESD_ASSIGNMENTS_VERSION = ea76c4749f20d5680ddd9a5685c8eb4caa844ca5
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,6 +18,10 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) -C $(@D)/server \
+		CC="$(TARGET_CC)" \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LDFLAGS="$(TARGET_LDFLAGS)"
 endef
 
 # DONE add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -26,6 +32,12 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket \
+		$(TARGET_DIR)/usr/bin/aesdsocket
+
+	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop \
+		$(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
